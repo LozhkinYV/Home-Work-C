692635f77bb3f1437ba7189838a2a9bf4182af10
unknown <user@tau-n-44.tau.local>
2022-10-03 00:21:02 +0400

    Ex02

---
+++ Seminar1/Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число1: ");
+string N11 = Console.ReadLine();
+int  N1 = int.Parse(N11);
+Console.Write("Введите число2: ");
+string N22 = Console.ReadLine();
+int  N2 = int.Parse(N22);
+
+if (N1 > N2) 
+{
+    Console.Write("max = ");
+    Console.Write(N1);
+    Console.Write(", ");
+    Console.Write("min = ");
+    Console.WriteLine(N2);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.Write(N2);
+    Console.Write(", ");
+    Console.Write("min = ");
+    Console.WriteLine(N1);
+}
