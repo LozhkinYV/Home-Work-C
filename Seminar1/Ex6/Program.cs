f335b3c5d66d2482654ad46af518ce6f50ca7449
unknown <user@tau-n-44.tau.local>
2022-10-03 00:45:52 +0400

    Ex06

---
+++ Seminar1/Ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string N11 = Console.ReadLine();
+double N1 = double.Parse(N11);
+if (N1%2 == 0) 
+{
+   Console.WriteLine("Четное"); 
+}
+else 
+{
+   Console.WriteLine("Нечетное");  
+}
