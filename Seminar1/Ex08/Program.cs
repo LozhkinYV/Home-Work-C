ea87cf29ab7ece722bff6a879bc7ac27c9fb5140
unknown <user@tau-n-44.tau.local>
2022-10-03 01:21:45 +0400

    Ex08

---
+++ Seminar1/Ex08/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string N11 = Console.ReadLine();
+int N = int.Parse(N11);
+int cout = 2;
+
+if (N >= cout)
+{
+    while (cout < N-1)
+    {
+       Console.Write(cout); 
+       Console.Write(", ");
+       cout = cout+2;
+    }
+    Console.Write(cout); // это нужно чтобы не выводило последнюю запятую
+}
+else 
+{
+    Console.Write("Задайте промежуток больше 1");
+}
