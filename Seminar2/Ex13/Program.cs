f3ceaa8b346838412ad93c1ed9a783a61b3d0db6
unknown <user@tau-n-44.tau.local>
2022-10-05 15:17:18 +0400

    Ex13

---
+++ Seminar2/Ex13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string a = Console.ReadLine();
+int b = a.Length;
+
+if (a[0] != '-' && b > 2)
+{
+    Console.WriteLine(a[2]); 
+} 
+else 
+{
+    if (a[0] == '-' && b > 3)
+    {
+        Console.WriteLine(a[3]);
+    }    
+        else 
+        {
+            Console.WriteLine("Третьей цифры нет");
+            }
+}
