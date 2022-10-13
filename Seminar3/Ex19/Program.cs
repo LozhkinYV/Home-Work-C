6d42a5c109a80d2392d5da377699a5ab0699581a
unknown <user@tau-n-44.tau.local>
2022-10-13 11:46:52 +0400

    Ex19

---
+++ Seminar3/Ex19/Program.cs
@@ -0,0 +1,20 @@
+﻿string Print(string massege)
+{
+	Console.Write(massege);
+	string value = Console.ReadLine();
+    value = value.Replace(" ", "");
+	return value;
+}
+
+string a = Print ("Введите число или слово: ");
+
+char[] rev = a.ToCharArray();
+Array.Reverse(rev);
+string b = new string(rev);
+
+
+
+if (a.ToLower() == b.ToLower()) {Console.WriteLine("Палиндром"); }
+else {Console.WriteLine("Не палиндром!");}
+
+
