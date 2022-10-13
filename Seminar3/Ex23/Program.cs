606228af1b89493e1d05b3a18d0c2002a8a386f1
unknown <user@tau-n-44.tau.local>
2022-10-13 14:54:25 +0400

    Ex23

---
+++ Seminar3/Ex23/Program.cs
@@ -0,0 +1,25 @@
+﻿int Input(string massege)
+{
+    Console.WriteLine(massege);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int InDegree(int val1, int val2)
+{
+    double value = Math.Pow(val1, val2);
+    int resalt = Convert.ToInt32(value);
+    return resalt;
+}
+
+int A = Input("Введите число A: ");
+int N = Input("Введите степень N: ");
+
+int count = 1;
+while (count <= A)
+    {
+        Console.Write(InDegree(count, N));
+        Console.Write(" ");
+        count++;
+    }
+
