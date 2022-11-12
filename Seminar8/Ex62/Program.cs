da366d4872cce5dd81506fa0dd444996c7692ba4
unknown <user@tau-n-44.tau.local>
2022-11-12 12:53:04 +0400

    Ex62

--- Seminar8/Ex62/Program.cs
+++ Seminar8/Ex62/Program.cs
@@ -38,6 +38,7 @@ int[,] FillArray(int[,] array) //создание рандомного двум�
     return array;
 }
 
+
 void PrintArray(int[,] array) //вывод двумерного массива
 {
     for (int row = 0; row < array.GetLength(0); row++)
@@ -50,8 +51,10 @@ void PrintArray(int[,] array) //вывод двумерного массива
     }
 }
 
-void FillImage(int[,] array, int r, int co, int k)
+
+/*void FillImage(int[,] array, int r, int co, int k)
 {
+     
     if (r >= array.GetLength(0)) r--;
     if (co >= array.GetLength(1)) co--;
     if (r < 0) r++;
@@ -60,22 +63,93 @@ void FillImage(int[,] array, int r, int co, int k)
     {
         array[r, co] += k;
         k++;
-        if (co==0)
         FillImage(array, r, co + 1, k);
-        if (co==array.GetLength(1))
         FillImage(array, r + 1, co, k);
-        if (r==0)
         FillImage(array, r, co - 1, k);
-        if (r==0)
         FillImage(array, r - 1, co, k);
+    }
+
+}*/
+
+int[,] FillMatrixSpiral(int[,] array)
+{
+    int k = 2; //заполнение массива
+    int r = 0; //позиция строки
+    int col = 0; //позиция столбца
+    int a = array.GetLength(1); // счетчик для движения вправо
+    int b = array.GetLength(0); // счетчик для движения вниз
+    int c = 0; // счетчик для движения вверх
+    int d = 0; // счетчик для движения влево
+   
+for (int l=0; l<array.GetLength(1)/2; l++)
+{
+    for (int i = col; i < a; i++)
+    {
+        if (array[r, col] == -1)
+        {
+            array[r, col]  += k;
+            k++;
+
+        }
+        col++;
+    }
+    col--;
+
+    for (int m = r; m < b; m++)
+    {
+        if (array[r, col] == -1)
+        {
+            array[r, col] += k;
+            k++;
+
+        }
+        r++;
+    }
+    r--;
+
+    for (int j = col; j >= c; j--)
+    {
+        if (array[r, col] == -1)
+        {
+            array[r, col] += k;
+            k++;
+
+        }
+        col--;
+    }
+    col++;
+    for (int n = r; n > d; n--)
+    {
+        if (array[r, col] == -1)
+        {
+            array[r, col] += k;
+            k++;
+
+        }
+        r--;
+    }
+    r++;
+    a--;
+    b--;
+    c++;
+    d++;
+    if (array.GetLength(1)%2!=0)
+    {
+     array[r, col+1] += k;
     }
 }
 
+    return array;
+}
+
+
+
+
 int row = Input("Введите количество строк и столбцов: ");
 int column = row;
-int rowMatrix = 0;
-int columnMatrix = 0;
-int kk = 2;
+//int rowMatrix = 0;
+//int columnMatrix = 0;
+//int kk = 2;
 
 Console.WriteLine();
 
@@ -83,7 +157,8 @@ int[,] array = new int[row, column];
 FillArray(array);
 PrintArray(array);
 Console.WriteLine();
-FillImage(array, rowMatrix, columnMatrix, kk);
+//FillImage(array, rowMatrix, columnMatrix, kk);
+FillMatrixSpiral(array);
 PrintArray(array);
 
 
