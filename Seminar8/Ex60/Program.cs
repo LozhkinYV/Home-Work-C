81cda975af58049b2341e39112312e45f255c151
unknown <user@tau-n-44.tau.local>
2022-11-12 17:50:07 +0400

    Ex60

--- Seminar8/Ex60/Program.cs
+++ Seminar8/Ex60/Program.cs
@@ -8,21 +8,64 @@
 26(1,0,1) 55(1,1,1)
 */
 
-int[,,] FillKub(int[,,] kub) //создание рандомного 3-х мерного массива depth
+int Input(string massege) // ввод целочисленных данных
 {
+    while (true)
+    {
+        Console.WriteLine(massege);
+        string value = Console.ReadLine();
+        if (int.TryParse(value, out int resalt))
+        {
+            return resalt;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Это не целое число! Попробуйте еще раз!");
+        }
+    }
+}
+
+int[] RandomNamber(int[] array, int minNamber, int maxNamber) //создаем массив из рандомных неповторяющихся чисел
+{
+    int count = 0;
+    for (int i = 0; i < maxNamber - minNamber; i++)
+    {
+        int a = new Random().Next(minNamber, maxNamber);
+        for (int j = 0; j < maxNamber - minNamber; j++)
+        {
+            if (a != array[j])
+                count++;
+            else { i--; break; }
+            if (count == 90)
+            {
+                array[i] = a;
+                count = 0;
+            }
+        }
+    }
+    return array;
+}
+
+int[,,] FillKub(int[,,] kub, int[] array) //создание рандомного 3-х мерного массива
+{
+    int i = 0;
     for (int width = 0; width < kub.GetLength(0); width++)
     {
         for (int height = 0; height < kub.GetLength(1); height++)
         {
             for (int depth = 0; depth < kub.GetLength(2); depth++)
-                kub[width, height, depth] = new Random().Next(10, 100);
+            {
+                kub[width, height, depth] = array[i];
+                i++;
+            }
         }
 
     }
     return kub;
 }
 
-void PrintKub(int[,,] kub) //вывод 3-х мерного массива depth
+void PrintKub(int[,,] kub) //вывод 3-х мерного массива 
 {
     for (int width = 0; width < kub.GetLength(0); width++)
     {
@@ -40,6 +83,20 @@ void PrintKub(int[,,] kub) //вывод 3-х мерного массива depth
 }
 
 
-int[,,] kub = new int[2, 2, 2];
-FillKub(kub);
+
+int min = 10;
+int max = 100;
+int a = Input("Каково ребро кубической матрицы: ");
+
+if (Convert.ToInt32(Math.Pow(a, 3)) > max-min)
+{
+    Console.WriteLine("Невозможно заполнить неповторяющимися числами");
+}
+else 
+{
+int[] randomNambers = new int[max-min];
+int[,,] kub = new int[a, a, a];
+RandomNamber(randomNambers, min, max);
+FillKub(kub, randomNambers);
 PrintKub(kub);
+}
