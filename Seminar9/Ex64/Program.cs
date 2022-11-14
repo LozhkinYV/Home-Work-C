5bc8598a93d342e7730ec4896b7f302eb621a747
unknown <user@tau-n-44.tau.local>
2022-11-14 23:45:07 +0400

    Ex64

---
+++ Seminar9/Ex64/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int Input(string massege) // ввод целочисленных данных
+{
+    while (true)
+    {
+        Console.WriteLine(massege);
+        string value = Console.ReadLine();
+        if (int.TryParse(value, out int resalt))
+        {
+            return resalt;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Это не целое число! Попробуйте еще раз!");
+        }
+    }
+}
+
+int Nambers (int  value)
+{
+    Console.Write($"{value}, ");
+    
+    if (value==2)
+    {
+        return 1;
+    }
+    else 
+    {
+        return Nambers (value-1);
+        
+    }
+}
+
+
+
+int N = Input("Введите целое число: ");
+Console.Write($"\"");
+Nambers (N);
+Console.Write($"1\"");
